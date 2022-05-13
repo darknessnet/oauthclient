a1efb91ccffa1eec8c8412f608aedf47c15a772f
Faltuuser <wick22660@gmail.com>
2022-05-13 11:34:50 +0530

    Add files via upload
    upload oauthclient

---
+++ src/translations/en/oauthclient.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * oauthclient plugin for Craft CMS 3.x
+ *
+ * login register
+ *
+ * @link      https://github.com/faltuuser/oauthclient
+ * @copyright Copyright (c) 2022 faltuuser
+ */
+
+/**
+ * oauthclient en Translation
+ *
+ * Returns an array with the string to be translated (as passed to `Craft::t('oauthclient', '...')`) as
+ * the key, and the translation as the value.
+ *
+ * http://www.yiiframework.com/doc-2.0/guide-tutorial-i18n.html
+ *
+ * @author    faltuuser
+ * @package   Oauthclient
+ * @since     1.0.0
+ */
+return [
+    'oauthclient plugin loaded' => 'oauthclient plugin loaded',
+];
